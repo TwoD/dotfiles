0fa2fcc8e4b430f9bffbe2ee346fa021fbee4d04
Henrik Danielsson <h.danielsson@gmail.com>
2021-09-02 15:54:33 +0200

    Skip profile compatibility.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,8 +5,6 @@ if test -f /usr/bin/gpg-connect-agent
 end
 set -gx BUILDDIR /tmp/makepkg
 set -gx EDITOR /usr/bin/vim
-# Fix /etc/profile.d/ compatibility.
-env -i HOME=$HOME dash -l -c 'export -p' | sed -e "/PWD/d; /PATH/s/'//g;/PATH/s/:/ /g;s/=/ /;s/^export/set -x/" | source
 set -gx PATH ~/bin /usr/local/bin $PATH
 # Allow local overrides.
 source ~/.config/fish/config.local.fish
